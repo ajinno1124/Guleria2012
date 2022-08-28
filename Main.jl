adf58fb4108b6d900e5658418cff43676e571678
ajinno1124 <jinno.asanosuke.36w@st.kyoto-u.ac.jp>
2022-08-28 18:35:24 +0900

    Save Progress

---
+++ Main.jl
@@ -0,0 +1,172 @@
+include("SkyrmeParams.jl")
+using Parameters
+using LinearAlgebra
+using Arpack
+
+@consts begin
+    # Prog. Theor. Exp. Phys. 2022, 083C01 (2022)
+    mpMeV=938.27208816
+    mnMeV=939.5654205
+    mAveMeV=(mpMeV+mnMeV)/2
+    mΛMeV=1115.683 #\pm 0.006 MeV
+    ħc=197.3269804
+
+    Nmesh=1000
+    rmax=15
+    nmax=10
+    lmax=7
+end
+
+mutable struct QuantumNumber
+    j::Float64
+    l::Int64
+    τ::Int64 #-1::p, 0::Lambda, 1::Ptoron
+end
+
+mutable struct SingleParticleState
+    QN::QuantumNumber
+    E::Float64
+    ψ::Vector{Float64}
+end
+
+function getrmesh()
+    h=rmax/(Nmesh-0.5)
+    rmesh=0.5*h:h:(Nmesh-0.5)*h
+    return rmesh
+end
+
+#Ay"+By'+Cy=ϵy
+
+function CoefA()
+end
+
+function CoefB()
+end
+
+function CoefC()
+end
+
+function calc_cij2(i,j,h,P)
+    cij = 0.0
+    if i==1
+        cij += ifelse(j==2,1.0,0.0)        
+        cij += ifelse(j==1,-P,0.0)
+    elseif i==Nmesh
+        #cij += ifelse(j==Nmesh-1,-1.0,0.0)
+        cij += ifelse(j==Nmesh-1,-2.0,0.0)
+        cij += ifelse(j==Nmesh,2.0,0.0)
+    else
+        cij += ifelse(j==i+1,1.0,0.0)
+        cij += ifelse(j==i-1,-1.0,0.0)
+    end
+    cij = cij/(2*h)
+    return cij
+end
+
+function calc_dij4(i,j,h)
+    dij = 0.0
+    if i==1
+        dij += ifelse(j==2,1.0,0.0)
+        dij += ifelse(j==1,P-2.0,0.0)
+    elseif i==Nmesh
+        #dij += ifelse(j==Nmesh,-2.0,0.0)        
+        #dij += ifelse(j==Nmesh-1,1.0,0.0)
+
+        dij += ifelse(j==Nmesh,2.0,0.0)
+        dij += ifelse(j==Nmesh-1,-5.0,0.0)
+        dij += ifelse(j==Nmesh-2,4.0,0.0)
+        dij += ifelse(j==Nmesh-3,-1.0,0.0)
+
+        #dij += ifelse(j==Nmesh,1.0,0.0)
+        #dij += ifelse(j==Nmesh-1,-2.0,0.0)
+        #dij += ifelse(j==Nmesh-2,1.0,0.0)
+    else
+        dij += ifelse(j==i+1,1.0,0.0)
+        dij += ifelse(j==i,-2.0,0.0)
+        dij += ifelse(j==i-1,1.0,0.0)
+    end
+    dij = dij/(h^2)
+    return dij
+end
+
+function MakeHmat(rmesh)
+    Hmat=spzeros(Nmesh,Nmesh)
+    h=rmesh[2]-rmesh[1]
+
+    for i in 1:Nmesh
+        Ai=CoefA()
+        Bi=CoefB()
+        Ci=CoefC()
+        Hmat[i,i]+=Ci
+        
+        for j in max(i-3,1):min(i+3,Nmesh)
+            cij=calc_cij2(i,j,h,l)
+            dij=calc_dij2(i,j,h,l)
+            if cij!=0 || dij!=0
+                Hmat[i,j]+=Ai*dij+Bi*cij
+            end
+        end
+    end
+
+    return Hmat
+end
+
+function NormFact(ψ,rmesh)
+end
+
+function CalcStates(QN::QuantumNumber)
+    States=SingleParticleState[]
+    rmesh=getrmesh()
+
+    offset=100
+    Hmat=MakeHmat(rmesh)
+    for i in 1:Nmesh
+        Hmat[i,i]+=offset
+    end
+
+    E,ψ=eigs(Hmat,nev=nmax)
+    E=real(E.-offset)
+    for i in 1:nmax
+        ψ[:,i]=real(ψ[:,i])./NormFact(real(ψ[:,i]),rmesh)
+        push!(States,SingleParticleState(QN,E[i],ψ[:,i]))
+    end
+
+    return States
+end
+
+function CalcAllStates(τ)
+    AllStates=SingleParticleState[]
+    for l in 0:lmax
+        for j in max(l-0.5,0.5):l+0.5
+            QN=QuantumNumber(j,l,τ)
+            States=CalcStates(QN)
+            AllStates=vcat(AllStates,States)
+        end
+    end
+
+    sort!(AllStates, by=x->x.E)
+
+    return AllStates
+end
+
+function occ()
+end
+
+function InitialCondition()
+end
+
+function Calc_ρ()
+end
+
+function Calc_τ()
+end
+
+function Calc_J()
+end
+
+function HF_iter()
+    InitialCondition()
+end
+
+
+
---
+++ SkyrmeParams.jl
@@ -0,0 +1,173 @@
+module NuclParamaters
+    export NuclParams, getParams
+    mutable struct NuclParams
+        t0::Float64
+        t1::Float64
+        t2::Float64
+        t3::Float64
+        x0::Float64
+        x1::Float64
+        x2::Float64
+        x3::Float64
+        σ::Float64
+        W0::Float64
+    end
+
+    function getParams(ParamType::String)
+        #SLy4
+        if ParamType=="SLy4"
+            t0=-2488.91
+            t1=486.82
+            t2=-546.39
+            t3=13777.0
+            x0=0.834
+            x1=-0.344
+            x2=-1.000
+            x3=1.354
+            σ=1/6
+            W0=123.0
+        end
+
+        #SLy5
+        if ParamType=="SLy5"
+            t0=-2484.88
+            t1=483.13
+            t2=-549.40
+            t3=13763.0
+            x0=0.778
+            x1=-0.328
+            x2=-1.000
+            x3=1.267
+            σ=1/6
+            W0=126.0
+        end
+
+        #SLy6
+        if ParamType=="SLy6"
+            t0=-2479.50
+            t1=462.18
+            t2=-448.61
+            t3=13763.0
+            x0=0.825
+            x1=-0.465
+            x2=-1.000
+            x3=1.355
+            σ=1/6
+            W0=122.0
+        end
+
+        #SLy7
+        if ParamType=="SLy7"
+            t0=-2482.41
+            t1=457.97
+            t2=-419.85
+            t3=13677.0
+            x0=0.846
+            x1=-0.511
+            x2=-1.000
+            x3=1.391
+            σ=1/6
+            W0=126.0
+        end
+
+        #SkM
+        if ParamType=="SkM*"
+            t0=-2645.00
+            t1=410.00
+            t2=-135.00
+            t3=15595.0
+            x0=0.09
+            x1=0.00
+            x2=0.00
+            x3=0.00
+            σ=1/6
+            W0=130.0
+        end
+
+        return NuclParams(t0,t1,t2,t3,x0,x1,x2,x3,σ,W0)
+    end
+end
+
+
+module LambdaParamaters
+    export LambdaParams, getParams
+    mutable struct LambdaParams
+        γ::Float64
+        u0::Float64
+        u1::Float64
+        u2::Float64
+        u3p::Float64
+        y0::Float64
+        y3::Float64
+    end
+
+    function getParams(ParamType::String)
+        if ParamType=="HPΛ1"
+            γ = 1
+            u0 = -326.395
+            u1 = 72.627
+            u2 = -8.584
+            u3p = 1746.041
+            y0 = -0.223
+            y3 = -0.389 
+        elseif ParamType=="HPΛ2"
+            γ = 1
+            u0 = -399.946
+            u1 = 83.426
+            u2 = 11.455
+            u3p = 2046.818
+            y0 = -0.486
+            y3 = -0.660
+        elseif ParamType=="HPΛ3"
+            γ =  1/3
+            u0 = -498.515
+            u1 = 65.203
+            u2 = 19.001
+            u3p = 995.832
+            y0 = -0.492
+            y3 = -0.444
+        elseif ParamType=="HPΛ4"
+            γ = 1/3
+            u0 = -475.584
+            u1 = 99.058
+            u2 = -20.890
+            u3p = 1375.172
+            y0 = -0.350
+            y3 = -0.724
+        elseif ParamType=="NΛ1"
+            γ = 1
+            u0 = -253.3250
+            u1 = 147.1264
+            u2 = -83.5843
+            u3p = 1684.9876
+            y0 = 0.5802
+            y3 = 0.4831
+        elseif ParamType=="OΛ1"
+            γ = 1
+            u0 = -236.5835
+            u1 = 116.8704
+            u2 = -112.8812
+            u3p = 1453.3493
+            y0 = 0.1271
+            y3 = -0.3110
+        elseif ParamType=="NΛ2"
+            γ = 1/3
+            u0 = -518.620
+            u1 = 82.0944
+            u2 = -19.9772
+            u3p = 1190.1894
+            y0 = -0.1392
+            y3 = 0.3126
+        elseif ParamType=="OΛ2"
+            γ = 1/3
+            u0 = -417.7593
+            u1 = 1.5460
+            u2 = -3.2617
+            u3p = 1102.2221
+            y0 = -0.3854
+            y3 = -0.5645
+        end
+
+        return LambdaParameters(γ,u0,u1,u2,u3p,y0,y3)
+    end
+end
